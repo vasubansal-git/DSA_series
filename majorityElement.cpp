7d73db7ea70ef94c186a9649c6a99074b33463a6
vasubansal-git <vasubansal8907@gmail.com>
2026-03-27 22:30:01 +0530

    Add Moore's algorithm

--- majorityElement.cpp
+++ majorityElement.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 vector<int> pairSum(vector<int> vec, int target){
@@ -43,7 +44,7 @@ int majorityElement(vector<int> nums){
     int n = nums.size();
 
     // Brute force approach.O(n * n)
-
+    
     for(int val : nums){
         int freq = 0;
         for(int el : nums){
@@ -57,6 +58,45 @@ int majorityElement(vector<int> nums){
         }
     }
     return -1;
+    
+
+    // Optimize approach.
+
+    //sort
+    sort(nums.begin(), nums.end());
+
+    //count freq
+    int freq = 1, ans = nums[0];
+    for(int i = 1; i < n; i++){
+        if(nums[i] == nums[i - 1]){
+            freq++;
+        } else {
+            freq = 1;
+            ans = nums[i];
+        }
+
+        if (freq > n / 2){
+            return ans;
+        }
+    }
+
+    return ans;  
+
+    //Moore's algorithm.
+
+    int freq = 0, ans = 0;
+
+    for(int i = 0; i < n; i++){
+        if(freq == 0){
+            ans = nums[i];
+        }
+        if(ans == nums[i]){
+            freq++;
+        } else {
+            freq--;
+        }
+    }
+    return ans;
 }
 
 int main(){
@@ -70,7 +110,7 @@ int main(){
     
     // Majority element in array.
 
-    vector<int> nums = {1, 1, 2, 2, 1};
+    vector<int> nums = {1, 1, 2, 2, 1, 2, 2};
 
     cout << "Majority element in the array is: " << majorityElement(nums) << endl;
 
